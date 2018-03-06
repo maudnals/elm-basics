0e2e844fbcbf81b5b69d32f91a61fe6b6f6e31d3
maudnals <maud.nalpas@gmail.com>
2018-03-06 08:36:20 +0100

    commented on function

--- Hello.elm
+++ Hello.elm
@@ -1,12 +1,21 @@
+-- any module in elm must start with this module declaration
 module Hello exposing (..)
 import Html exposing (text, button, input)
 
 main = text "Hello" 
 
+-- named function. first line = signature, then = implementation
 addOne : Int -> Int
 addOne x =
   x + 1
 
+-- note how whitespace is used to denote function application
+-- and how it's based on partial application / currying (in Elm all functions take exactly one argument (arity =1) and return a result. This result can be another function.)
+-- function application associate to the LEFT hence no () needed
+
 sum : Int -> Int -> Int
 sum x y =
   x + y
+
+-- anonymous function
+-- \x -> x + 1
