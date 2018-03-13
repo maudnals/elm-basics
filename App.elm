fc48b056b3a2577ecad56fa3d5693235460a3a79
maudnals <maud.nalpas@gmail.com>
2018-03-13 18:20:25 +0100

    trained otherapp

--- App.elm
+++ App.elm
@@ -6,23 +6,31 @@ main = text "hi"
 
 -- MODEL
 
+
 type alias Model = 
   String
 
 -- no arrow for signature just means no input value (only a return value)
 -- and the () are for a tuple
-init : (Model, Cmd Message)
+init : ( Model, Cmd Msg )
 init =
-  ("initialModel", Cmd.none)
+  ( "initialModel", Cmd.none )
+
+
+
+
 
 
 -- MESSAGES
+
+
 -- a message is something that happens in our app, that our app responds to
-type Message = 
+type Msg = 
   NoOp
 
--- here not so interesting cause no interactions => no messages. Other example
--- type Message = Collapse | Expand
+-- here not so interesting cause no interactions => no Msgs. Other example
+-- type Msg = Collapse | Expand
+
 
 
 
@@ -30,15 +38,25 @@ type Message =
 
 -- VIEW
 
--- Message is a union type here used as type variable
-view : Model -> Html Message
+-- Msg is a union type here used as type variable
+view : Model -> Html Msg
 view myModel =
   div []
     []
 
 
 
+
+
+
 -- UPDATE
+
+update: Msg -> Model -> ( Model, Cmd Msg)
+update msg model =
+  case msg of
+      NoOp -> ( model, Cmd.none)
+
+
 
 
 
