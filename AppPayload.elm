c19d0c0622322ba45f5709f3a8e6c80f5bf043b6
maudnals <maud.nalpas@gmail.com>
2018-03-19 08:33:09 +0100

    with payload

--- AppPayload.elm
+++ AppPayload.elm
@@ -1,25 +1,49 @@
 module AppPayload exposing (..)
-import Html exposing (div, program, text)
+import Html exposing (button, div, Html, program, text)
+import Html.Events exposing (onClick)
 
-main = text "hi"
 
 -- model
 type alias Model = 
   Int
+
 
 -- init
 init : (Int, Cmd Msg)
 init =
   (0, Cmd.none)
 
+
 -- message
-type Msg = Increment Int | Decrement Int
+type Msg = Increment Int
 
 -- view
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick (Increment 2)] [text "+"]]
 
 -- update
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+      Increment howMuch->
+          (model + howMuch, Cmd.none)
+
 
 -- subscriptions
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
+
+
+-- main
+main : Program Never Model Msg
+main = 
+  program 
+    {
+      init = init,
+      view = view,
+      update = update,
+      subscriptions = subscriptions 
+    }
