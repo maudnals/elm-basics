e11c12f1cc304edbc345edcd1b4ecab5d83d14ea
maudnals <maud.nalpas@gmail.com>
2018-03-20 08:16:40 +0100

    added msg union type

--- Commands.elm
+++ Commands.elm
@@ -5,6 +5,42 @@ import Html exposing (div, Html, text)
 
 -- Why?
 -- Elm is a functional language. So, every function must return a value, and function side effects are forbidden. So in order to model side effects: the function will return the name of the desired command.
--- The main recipient of this command value is Html.Program (who will deal with the dirty side effect work).
+-- The main recipient of this command value is Html.Program - and then, the update method of Html.program who contains the logic to execute the command (but who deals with the dirty side effect work?).
 
+
+-- model
+type alias Model = 
+  Int
+
+init : (Model, Cmd Msg)
+init = 
+  (1, Cmd.none)
+
+
+-- view
+
+
+-- update
+
+
+
+-- msg
+-- a msg is sthg that happens in our app, that we want to react to.
+-- onResult is how we "listen" to the random number being generated - we can't use a subscription here since subscriptions are for user/external inputs!
+type Msg = 
+  Roll
+  | OnResult Int
+
+
+
+-- subscriptions
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+-- main
 main = text "hi"
+
+
+-- note how subscriptions and DOM Html msg stuff do the same thing.
+-- note how update and init have a similar role (init is just an update without input value)
